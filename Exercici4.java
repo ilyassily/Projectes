506667189fdc4f4c7010dfa63db61b4e7b2d26cc
Mehdi Aroun <25mohamed.aroun@inscastellet.cat>
2025-11-07 16:00:50 +0100

    Initial commit

---
+++ Exercici4.java
@@ -0,0 +1,22 @@
+package Joc;
+
+import java.util.Scanner;
+
+public class Exercici4 {
+    public static void main(String[] args) {
+        Scanner llegir = new Scanner(System.in);
+
+        int random = (int) (Math.random() * 30) + 1;
+
+        System.out.println("Endevina el número (entre 1 i 30):");
+        int numero = llegir.nextInt();
+
+        if (numero == random) {
+            System.out.println("L'has encertat!");
+        } else {
+            System.out.println("No l'has encertat. El número correcte era: " + random);
+        }
+
+        llegir.close();
+    }    
+}
